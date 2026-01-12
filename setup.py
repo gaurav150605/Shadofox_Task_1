39908e8ca74cc265303bf1ca79d30f0a6ecfa48d
Gaurav150605 <gaurav1898@gmail.com>
2026-01-13 00:28:44 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+
+setup(
+name='Shadofox_Task_1',
+version='0.0.1',
+author='Gaurav',
+author_email='gauravshinde1898@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+)
